ba9f0fc58839a4c462da4b5cce770a1d292439aa
luis-zaldivar <lalozaldivarbarza@gmail.com>
2019-12-05 15:35:02 -0600

    segundo intento

--- main.cpp
+++ main.cpp
@@ -1,129 +1,78 @@
 #include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char** argv) {
+struct NODO{
+    string pedido;
+    NODO*siguiente;
+}; NODO* cola=NULL;
+
+
+
+void agregar(string);
+void eliminar();
+string visualizar();
+
+int main(){
+	int opcion, rep=1;
+	string orden, ped;
+	while (rep==1){
+      cout<<"\t\t ...::MENU::...\n 1.-Inserter pedido \n 2.-Mostrar pedido \n 3.-Elimnar\n 4.-salir\n";
+      cin>>opcion;
+      switch(opcion){
+          case 1:
+              cout<<"Introduzca el nombre del pedido: "; 
+              getline(cin,orden); ///PARA LIMPIAR EL BUFFER
+              getline(cin,orden); ///Lee una linea con espacios
+              agregar(orden);
+              break;
+        case 2:
+            ped=visualizar();
+            if(ped!="No hay pedidos menso xd")
+                cout<<"El primer pedido  es: "<<ped;
+            else
+                cout<<"La lista de pedidos esta vacio"<<endl;
+            break;
+          case 3:
+            cout<<"Se esta eliminando la el primer pedido de la cola "<<endl;
+            eliminar();
+            break;
+        default:
+            cout<<"Desea repetir el proceso? SI=1/NO=0 "; cin>>rep;
+      }
+  }
 	
-	int fecha,dia,opcion,mes;
-	cout<<"\t\t ...::MENU::...\n 1.-Inserter pedido \n 2.-Eliminar pedido \n 3.-Buscar pedido\n 4.-salir\n";
-	cin>>opcion;
-	do{
-	switch(opcion){
-			case 1:
-			cout<<"inserte el mes de llegada: ";
-			cin>>mes;
-				if (mes==1){
-					cout<<"inserte el dia de llegada: ";
-					cin>>dia;
-					if(dia<=0){
-						cout<<"error dia incorrecto\n";
-					}else if(dia>31){
-						cout<<"error dia incorrecto\n";
-					}
-					cout<<"inserte el nombre del pedido";
-				}
-				if (mes==2){
-					cout<<"inserte el dia de llegada: ";
-					cin>>dia;
-					if(dia<=0){
-						cout<<"error dia incorrecto\n";
-					}else if(dia>29){
-						cout<<"error dia incorrecto\n";
-					}
-				}
-				if (mes==3){
-					cout<<"inserte el dia de llegada: ";
-					cin>>dia;
-					if(dia<=0){
-						cout<<"error dia incorrecto\n";
-					}else if(dia>31){
-						cout<<"error dia incorrecto\n";
-					}
-				}if (mes==4){
-					cout<<"inserte el dia de llegada: ";
-					cin>>dia;
-					if(dia<=0){
-						cout<<"error dia incorrecto\n";
-					}else if(dia>30){
-						cout<<"error dia incorrecto\n";
-					}
-				}if (mes==5){
-					cout<<"inserte el dia de llegada: ";
-					cin>>dia;
-					if(dia<=0){
-						cout<<"error dia incorrecto\n";
-					}else if(dia>30){
-						cout<<"error dia incorrecto\n";
-					}
-				}if (mes==6){
-					cout<<"inserte el dia de llegada: ";
-					cin>>dia;
-					if(dia<=0){
-						cout<<"error dia incorrecto\n";
-					}else if(dia>30){
-						cout<<"error dia incorrecto\n";
-					}
-				}if (mes==6){
-					cout<<"inserte el dia de llegada: ";
-					cin>>dia;
-					if(dia<=0){
-						cout<<"error dia incorrecto\n";
-					}else if(dia>30){
-						cout<<"error dia incorrecto\n";
-					}
-				}if (mes==7){
-					cout<<"inserte el dia de llegada: ";
-					cin>>dia;
-					if(dia<=0){
-						cout<<"error dia incorrecto\n";
-					}else if(dia>31){
-						cout<<"error dia incorrecto\n";
-					}
-				}if (mes==8){
-					cout<<"inserte el dia de llegada: ";
-					cin>>dia;
-					if(dia<=0){
-						cout<<"error dia incorrecto\n";
-					}else if(dia>31){
-						cout<<"error dia incorrecto\n";
-					}
-				}if (mes==9){
-					cout<<"inserte el dia de llegada: ";
-					cin>>dia;
-					if(dia<=0){
-						cout<<"error dia incorrecto\n";
-					}else if(dia>30){
-						cout<<"error dia incorrecto\n";
-					}
-				}if (mes==10){
-					cout<<"inserte el dia de llegada: ";
-					cin>>dia;
-					if(dia<=0){
-						cout<<"error dia incorrecto\n";
-					}else if(dia>31){
-						cout<<"error dia incorrecto\n";
-					}
-				}if (mes==11){
-					cout<<"inserte el dia de llegada: ";
-					cin>>dia;
-					if(dia<=0){
-						cout<<"error dia incorrecto\n";
-					}else if(dia>30){
-						cout<<"error dia incorrecto\n";
-					}
-				}if (mes==12){
-					cout<<"inserte el dia de llegada: ";
-					cin>>dia;
-					if(dia<=0){
-						cout<<"error dia incorrecto\n";
-					}else if(dia>31){
-						cout<<"error dia incorrecto\n";
-					}
-				}
-			
-			
-			break;
-	}	
-	}while (opcion==4);
+	
 	return 0;
+}
+void agregar(string orden){
+    if (cola==NULL){
+        cola=new NODO();
+        cola->pedido=orden;
+        cola->siguiente=NULL;
+    }else{
+        NODO *aux=cola;
+        while(aux->siguiente!=NULL){
+            aux=aux->siguiente;
+        }
+        aux->siguiente=new NODO();
+        aux=aux->siguiente;
+        aux->pedido=orden;
+        aux->siguiente=NULL;
+    }
+}
+
+void eliminar(){
+    if (cola==NULL) return;
+    NODO* aux=cola;
+    cola=cola->siguiente;
+    delete aux;
+}
+
+string visualizar(){
+    if (cola!=NULL){
+        return cola->pedido;
+    }
+    return "No hay pedidos menso ";
 }
